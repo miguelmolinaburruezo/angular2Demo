73a4b2ba415ec100655469a40fca414736cb0d49
miguel molina <miguelmolinaburruezo2@gmail.com>
2018-11-27 10:12:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular2Demo';
+  agreed = 0;
+  disagreed = 0;
+  voters = ['superman', 'spiderman', 'deadpool', 'luke cage', 'jessica jones', 'quake'];
+  onVoted(event) {
+    console.log('onvoted', event);
+    if (event) {
+      this.agreed++;
+    } else {
+      this.disagreed++;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoterComponent } from './voter/voter.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { VoterComponent } from './voter/voter.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
